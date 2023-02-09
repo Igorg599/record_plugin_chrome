3bf90fd94bb7af7ff902a0329e9fa08a947dcb04
Гордеев Игорь Юрьевич <igorg599@gmail.com>
2023-02-09 18:03:18 +0500

    chore: плавное появление iframe

--- js/content-script.js
+++ js/content-script.js
@@ -1,31 +1,42 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg === "initialization") {
-    addEventListener(evtFromPage, (e) => {
-      chrome.runtime.sendMessage(e.detail)
-      sendResponse(e.detail), { once: true }
-    })
-    dispatchEvent(new Event(evtToPage))
-  }
-  if (msg === "close") {
-    iframe.style.width = "0px"
-  }
-  if (msg == "open") {
-    iframe.style.width = "100vw"
+chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
+  const iframePlugin = document.querySelector("#record_plugin")
+  switch (msg) {
+    case "initialization": {
+      addEventListener(evtFromPage, (e) => {
+        chrome.runtime.sendMessage(e.detail)
+        sendResponse(e.detail), { once: true }
+      })
+      dispatchEvent(new Event(evtToPage))
+      break
+    }
+    case "close": {
+      iframePlugin?.remove()
+      break
+    }
+    case "open": {
+      const iframe = document.createElement("iframe")
+      iframe.id = "record_plugin"
+      iframe.style.opacity = "0.1"
+      iframe.style.height = "100vh"
+      iframe.style.width = "100vw"
+      iframe.style.position = "fixed"
+      iframe.style.top = "0px"
+      iframe.style.right = "0px"
+      iframe.style.zIndex = "9000000000000000000"
+      iframe.allow = "display-capture"
+      iframe.frameBorder = "none"
+      iframe.src = chrome.runtime.getURL("../html/iframe.html")
+      document.body.appendChild(iframe)
+      let timer = setInterval(() => {
+        if (iframe.style.opacity === "0.6") {
+          clearInterval(timer)
+          return
+        }
+        iframe.style.opacity = `${+iframe.style.opacity + 0.02}`
+      }, 25)
+      break
+    }
+    default:
+      throw new Error("Error message!")
   }
 })
-
-var iframe = document.createElement("iframe")
-iframe.style.background = "green"
-iframe.style.borderRadius = "11px"
-iframe.style.opacity = "0.7"
-iframe.style.height = "100vh"
-iframe.style.width = "0px"
-iframe.style.position = "fixed"
-iframe.style.top = "0px"
-iframe.style.right = "0px"
-iframe.style.zIndex = "9000000000000000000"
-iframe.allow = "display-capture"
-iframe.frameBorder = "none"
-iframe.src = chrome.runtime.getURL("../html/iframe.html")
-
-document.body.appendChild(iframe)
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, "close")
+  })
   setTimeout(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, "open")
