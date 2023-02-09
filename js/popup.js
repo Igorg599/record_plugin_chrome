333fdf5ec6c72f215f809b85a6e9ad21a73c3db9
Гордеев Игорь Юрьевич <igorg599@gmail.com>
2023-02-09 15:18:03 +0500

    chore: add close listener

--- js/popup.js
+++ js/popup.js
@@ -52,4 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
     await newMedia.getFlowVideo()
     await newMedia.getFlowAudio()
   })
+
+  document.getElementById("close").addEventListener("click", () => {
+    chrome.tabs.getCurrent((tab) => {
+      chrome.tabs.sendMessage(tab.id, "close")
+    })
+  })
 })
